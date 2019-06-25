e0505237bb5bfebee6b98c67a66a85995c3dcd6f
Audra Tallman <audi.tallman@gmail.com>
2019-06-25 17:50:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ let mealId = 0;
 let customerId = 0; 
 let deliveryId = 0;
 
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+    store.neighborhood.push(this);
+  }
+}
+
+
 class Customer {
   constructor(name) {
     this.name = name;
