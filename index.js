5e51b47aeeb4a5f20c5e800693e2bd9d4256149c
Audra Tallman <audi.tallman@gmail.com>
2019-06-25 18:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ class Meal {
     }
     deliveries() {
     return store.deliveries.filter(delivery => {
-      return delivery.mealId == this.id;
+      return delivery.mealId === this.id;
      });
   }
 }
